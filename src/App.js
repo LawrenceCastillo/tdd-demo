446a70c3c07fb94320c19601175d8c3d85e36b77
LawrenceCastillo <lawrence.g.castillo@gmail.com>
2020-06-28 20:13:28 -0400

    adds test for app button interaction and result

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
-
+  constructor(props){
+    super(props)
+    this.state = {
+      counter: 0
+    }
+  }
   render() {
     return (
       <div data-test='component-app'>
         <h1 data-test='counter-display'>
-          The counter is currently 
+          The counter is currently {this.state.counter}
         </h1>
-        <button data-test='increment-button'>
+        <button 
+          data-test='increment-button'
+          onClick={() => this.setState({ counter: this.state.counter + 1 })}
+        >
           Increment counter
         </button>
       </div>
